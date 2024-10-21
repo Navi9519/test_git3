b3d54f9b31b3ac536df96f439953c849e028a67d
Navi9519 <105633246+Navi9519@users.noreply.github.com>
2024-10-21 09:19:41 +0200

    first commit

---
+++ testest/testestApp.swift
@@ -0,0 +1,17 @@
+//
+//  testestApp.swift
+//  testest
+//
+//  Created by Ivan Dedic on 2024-10-21.
+//
+
+import SwiftUI
+
+@main
+struct testestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
